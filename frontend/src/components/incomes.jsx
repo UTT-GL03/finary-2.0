2bce8da6f05366a47315b63d20d2e6c2a7f28127
mcthequeen <jeremrichard20@gmail.com>
2025-10-20 17:18:22 +0200

    added outcomes and incomes chatrs and table

--- frontend/src/components/incomes.jsx
+++ frontend/src/components/incomes.jsx
@@ -1,17 +1,43 @@
-import { data } from '../../lib/data';
+import { data } from "../../lib/data";
+
+import { LineChart } from "./line-chart";
+import { CustomTable } from "./custom-table";
 
 export function Incomes() {
-    const incomes = data.expenses.filter((expense) => expense.amount > 0);
-    return (
-        <div>
-            <h1>Incomes</h1>
-            <ul>
-                {incomes.map((income) => (
-                    <div key={income.id}>
-                        <li > + {income.amount} - {income.created_at} - {income.category}</li>
-                    </div>
-                ))}
-            </ul>
-        </div>
-    )
+  const incomes = data.expenses.filter((expense) => expense.amount > 0);
+
+  const incomeChartData = {};
+
+    const tableContent =[];
+
+    incomes.forEach((income) => {
+        tableContent.push([income.amount, income.created_at, income.category]);
+    })
+
+  const taleData = {
+    headers: ['amount', 'date', 'category'],
+    content: tableContent
+  }
+
+  incomes.forEach((income) => {
+    
+    const date = income.created_at.split(" ")[0];
+
+    
+    if (!incomeChartData[date]) {
+      incomeChartData[date] = 0;
+    }
+    incomeChartData[date] += income.amount;
+  });
+
+  return (
+    <div>
+      <h1>Incomes</h1>
+      <div >
+      <LineChart chartData={incomeChartData} />
+
+      <CustomTable tableData={taleData} />
+      </div>
+    </div>
+  );
 }
