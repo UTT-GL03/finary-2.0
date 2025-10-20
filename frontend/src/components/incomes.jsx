8defaa962b0fbc88276fc4f93f66991d805836a3
mcthequeen <jeremrichard20@gmail.com>
2025-10-20 16:22:48 +0200

    added data in pages

--- frontend/src/components/incomes.jsx
+++ frontend/src/components/incomes.jsx
@@ -1,7 +1,17 @@
-export  function Incomes() {
+import { data } from '../../lib/data';
+
+export function Incomes() {
+    const incomes = data.expenses.filter((expense) => expense.amount > 0);
     return (
         <div>
             <h1>Incomes</h1>
+            <ul>
+                {incomes.map((income) => (
+                    <div key={income.id}>
+                        <li > + {income.amount} - {income.created_at} - {income.category}</li>
+                    </div>
+                ))}
+            </ul>
         </div>
     )
 }
--- frontend/src/components/outcomes.jsx
+++ frontend/src/components/outcomes.jsx
@@ -1,7 +1,17 @@
+import { data } from '../../lib/data';
+
 export function Outcomes() {
+    const outcomes = data.expenses.filter((expense) => expense.amount < 0);
     return (
         <div>
             <h1>Outcomes</h1>
+            <ul>
+                {outcomes.map((outcome) => (
+                    <div key={outcome.id}>
+                        <li > {outcome.amount} - {outcome.created_at} - {outcome.category}</li>
+                    </div>
+                ))}
+            </ul>
         </div>
-    )
+    )   
 }
