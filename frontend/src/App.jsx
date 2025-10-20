bb0db5dc1fef0c507154bbbddb4662a27a605a16
mcthequeen <jeremrichard20@gmail.com>
2025-10-20 15:52:25 +0200

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,73 +1,25 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import reactLogo from "./assets/react.svg";
 
-import { data } from "../lib/data";
+
+import "./App.css";
+import { Dashboard } from "./components/dashboard";
+import { Detail } from "./components/detail";
 
 function App() {
-  const userName = "Nathan";
-
-  const userId = data.users.find((user) => user.user == userName)?.user_id;
-
-  const expenses = data.expenses.filter(
-    (expense) => expense.user_id === userId
-  );
-
-  const incomes = expenses.filter((expense) => expense.amount > 0);
-  const outcomes = expenses.filter((expense) => expense.amount < 0);
-
-  let totalIncome = 0;
-  let totalOutcome = 0;
-
-  incomes.forEach((income) => (totalIncome += income.amount));
-  outcomes.forEach((outcome) => (totalOutcome += outcome.amount));
-
-  const incomesCategoriesCount = {};
-  const outcomesCategoriesCount = {};
-  incomes.forEach((income) => {
-    incomesCategoriesCount[income.category] =
-      (incomesCategoriesCount[income.category] || 0) + income.amount;
-  });
-
-  outcomes.forEach((outcome) => {
-    outcomesCategoriesCount[outcome.category] =
-      (outcomesCategoriesCount[outcome.category] || 0) + outcome.amount;
-  });
-  console.log("cat : ", incomesCategoriesCount);
-
   return (
-    <div>
-      <h1>Bonjour {userName}</h1>
-      <h1>Data</h1>
-      <p>Balance: {totalIncome + totalOutcome}</p>
-      <ul>
-        {expenses.map((expense) => (
-          <div>
-            <li key={expense.id}>{expense.amount}</li>
-            <li key={expense.id}>{expense.created_at}</li>
-          </div>
-        ))}
-      </ul>
-      <div>
-        <h2>Income: </h2>
-        <p>Total : {totalIncome}</p>
-        {Object.entries(incomesCategoriesCount).map(([key, value]) => (
-          <div key={key}>
-            <span>{key}: </span>
-            <span>{value}</span>
-          </div>
-        ))}
-      </div>
-      <div>
-        <h2>Outcome: </h2>
-        <p>Total : {totalOutcome}</p>
-        {Object.entries(outcomesCategoriesCount).map(([key, value]) => (
-          <div key={key}>
-            <span>{key}: </span>
-            <span>{value}</span>
-          </div>
-        ))}
-      </div>
-      <div></div>
-    </div>
+    <BrowserRouter>
+      <header>
+        <h1>
+          <img src={reactLogo} />
+          Finary 2.0
+        </h1>
+      </header>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path=":id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
